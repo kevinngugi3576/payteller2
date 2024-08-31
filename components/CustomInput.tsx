9a73be3eb155b6d326d1ccd61396b9845bdbe56b
kevinngugi3576 <kevinngugi197@gmail.com>
2024-08-31 16:34:17 +0300

    implement id atributes

--- components/CustomInput.tsx
+++ components/CustomInput.tsx
@@ -1,49 +1,48 @@
 import React from "react";
 import {
-  FormField,
   FormControl,
+  FormField,
+  FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { FieldPath, Control } from "react-hook-form";
-import { z } from "zod";
-import { authFormSchema } from "@/lib/utils";
 
-const formSchema = authFormSchema("sign-up");
-
-type CustomInputProps = {
-  control: Control<z.infer<typeof formSchema>>;
-  name: FieldPath<z.infer<typeof formSchema>>;
+interface CustomInputProps {
+  control: any;
+  name: string;
   label: string;
   placeholder: string;
-  id:string
-  autocomplete: string;
-};
+  id: string; // Add this line
+  autocomplete?: string;
+}
 
-const CustomInput = ({ control, name, label, placeholder, id, autocomplete }: CustomInputProps) => {
-
+const CustomInput: React.FC<CustomInputProps> = ({
+  control,
+  name,
+  label,
+  placeholder,
+  id, // Add this line
+  autocomplete,
+}) => {
   return (
     <FormField
       control={control}
       name={name}
       render={({ field }) => (
-        <div className="form-item" >
-          <FormLabel  className="form-label" id={id}>
-            {label}
-          </FormLabel>
-          <div className="flex flex-col w-full">
-            <FormControl>
-              <Input
-                placeholder={placeholder}
-                className="input-class"
-                type={name === "password" ? "password" : "text"}
-                {...field}
-              />
-            </FormControl>
-            <FormMessage className="mt-2 form-message" />
-          </div>
-        </div>
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input
+              placeholder={placeholder}
+              {...field}
+              id={id} // Add this line
+              autoComplete={autocomplete}
+              className="input-class"
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
       )}
     />
   );
