90f187b39a204fe2097214434c7a6030e1357576
kevinngugi3576 <kevinngugi197@gmail.com>
2024-09-28 02:00:32 +0300

    implement next.js cookie handler

--- app/(root)/MyBanks/page.tsx
+++ app/(root)/MyBanks/page.tsx
@@ -1,26 +1,26 @@
 
-"use client"
-import Landing from "@/components/Landing";
-import React, { Suspense, useEffect, useState } from "react";
+// "use client"
+// import Landing from "@/components/Landing";
+// import React, { Suspense, useEffect, useState } from "react";
 
 
-const MyBanks = () => {
-  const [loading, setLoading] = useState(true);
+// const MyBanks = () => {
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setLoading(false); // Set loading to false after 1 second
-    }, 1000);
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       setLoading(false); // Set loading to false after 1 second
+//     }, 1000);
 
-    return () => clearTimeout(timer); // Clean up the timer
-  }, [loading]);
+//     return () => clearTimeout(timer); // Clean up the timer
+//   }, [loading]);
 
-  return (
-    <Suspense fallback={<Landing />}>
-      {loading ? <Landing /> : <Landing />}
-    </Suspense>
-  );
-};
+//   return (
+//     <Suspense fallback={<Landing />}>
+//       {loading ? <Landing /> : <Landing />}
+//     </Suspense>
+//   );
+// };
 
-export default MyBanks;
+// export default MyBanks;
 
